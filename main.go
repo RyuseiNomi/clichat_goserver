a85e2e23a2e1628d7f7497ef44e7999f9f1f156a
RyuseiNomi <kanontonto1010@gmail.com>
2020-08-12 19:25:18 +0900

    create process to create a room

--- main.go
+++ main.go
@@ -5,11 +5,21 @@ import (
 	"net/http"
 )
 
+var serverPort = ":8000"
+
 func main() {
+
+	// ルームの生成
+	r := NewRoom()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`hello`))
 	})
+	http.Handle("/room", r)
 
+	// ルームの開始。これ以降、ルームはコマンドが終了するまで入退場を監視する
+	go r.Run()
+
+	log.Println("Webサーバの起動")
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
 	}
